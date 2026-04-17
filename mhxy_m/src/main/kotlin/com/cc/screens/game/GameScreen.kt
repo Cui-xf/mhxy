753ea0cecc3c834827d76c3ca4e6656efd9686b3
cxf <cuixiaofeng@yuanfudao.com>
2026-04-17 16:03:20 +0800

    A

--- mhxy_m/src/main/kotlin/com/cc/screens/game/GameScreen.kt
+++ mhxy_m/src/main/kotlin/com/cc/screens/game/GameScreen.kt
@@ -5,9 +5,15 @@ import com.cc.screens.AbstractScreen
 class GameScreen : AbstractScreen() {
     private val screenMap = ScreenMap(this.assetLoader)
     private val player = Player(this.assetLoader, screenMap)
+    private val npc = listOf(
+        Npc(this.assetLoader, screenMap),
+    )
 
     override fun update(delta: Float) {
         screenMap.render(batch, sr, 0f, 0f, VIRTUAL_W, VIRTUAL_H, delta)
         player.render(batch, sr, 0f, 0f, VIRTUAL_W, VIRTUAL_H, delta)
+        for (npc in npc) {
+            npc.render(batch, sr, 0f, 0f, VIRTUAL_W, VIRTUAL_H, delta)
+        }
     }
 }
--- mhxy_m/src/test/kotlin/FindByResId.kt
+++ mhxy_m/src/test/kotlin/FindByResId.kt
@@ -1,12 +1,10 @@
 import java.io.File
 
 val str = listOf(
-    "24_-311472867.pic",
-    "26_-1065726095.pic",
-    "19_-147189103.pic",
+    "94_51529459.pic",
 )
 
-val file = "${destRoot}/assets/rpg/role"
+val file = "${destRoot}/assets/rpg/petfight"
 fun main() {
     for (item in File(file).listFiles()) {
         val readText = item.readText()
